06b49377743dbb3ed0cef82359456e6446ef1b40
Frank Atencio Loreth <48834633+FrankAtencioLoreth@users.noreply.github.com>
2025-03-18 21:49:35 -0500

    Add readme.md

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -5,7 +5,6 @@ import io.cucumber.java.Scenario;
 import org.junit.AfterClass;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import pages.BasePage;
 
 public class Hooks extends BasePage {
